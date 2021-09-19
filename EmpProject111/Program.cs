7622116c321f44cffe6fe8d4fcdc283ecf20d473
mounika <mounaadagatla@gmail.com>
2021-09-19 18:09:11 +0530

    [Refactor]:emp status added

---
+++ EmpProject111/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace EmpProject111
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welocome to Employee Wage computation");
+            WageCaluclation empWage = new WageCaluclation();
+            empWage.Attendance();
+            Console.ReadKey();
+        }
+    }
+}
